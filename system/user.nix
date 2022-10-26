0fe3b7d9b198f4f834b5bb99dbf4283bdf312c63
Raphael Groemmer <r.groemmer@yahoo.de>
2022-10-26 14:41:12 +0200

    remove podman

--- system/user.nix
+++ system/user.nix
@@ -3,14 +3,6 @@
 
   programs = { zsh.enable = true; };
 
-  virtualisation = {
-    podman = {
-      enable = true;
-      dockerCompat = true;
-      dockerSocket.enable = true;
-    };
-  };
-
   users.users.${user} = {
     extraGroups = [
       "wheel" # sudo
