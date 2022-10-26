0c944f86e8a9029696f48b7fe5736492e4cd45d1
Raphael Groemmer <mail@rapsn.me>
2022-10-26 18:07:09 +0200

    add git config

--- home/shell/git.nix
+++ home/shell/git.nix
@@ -1,5 +1,40 @@
 { pkgs, ... }: {
   programs.git = {
-    #TODO add gitconfig for work and private
+    enable = true;
+    userEmail = "mail@rapsn.me";
+    userName = "Raphael Groemmer";
+
+    delta.enable = true;
+    ignores = [
+      ".idea" ".vs" ".vsc" ".vscode" # ide
+      ".DS_Store" # mac
+      "node_modules" "npm-debug.log" # npm
+      "__pycache__" "*.pyc" # python
+      ".ipynb_checkpoints" # jupyter
+      "__sapper__" # svelte
+    ];
+    
+    extraConfig = {
+      init = { defaultBranch = "main"; };
+      pull = {
+        ff = false;
+        commit = false;
+        rebase = true;
+      };
+      push.autoSetupRemote = true;
+      url = {
+        "ssh://git@github.com" = { insteadOf = "https://github.com"; };
+      };
+      delta = {
+        line-numbers = true;
+      };
+    };
+
+    includes = [{
+      condition = "gitdir:~/Projects/SKE/**";
+      contents = {
+        user.email = "raphael.groemmer@stackit.de";
+      };
+    }];
   };
 }
