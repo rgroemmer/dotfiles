1a8ce943d65e14df8d0d3a34e4f6561c73808b83
Raphael Grömmer <38243306+rgroemmer@users.noreply.github.com>
2024-06-10 22:45:09 +0200

    Zsh refactor (#5)

--- home-manager/cli/shell/zsh.nix
+++ home-manager/cli/shell/zsh.nix
@@ -1,12 +1,21 @@
-{ pkgs, lib, ... }:
 {
   programs.zsh = {
     enable = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    syntaxHighlighting.catppuccin.enable = true;
     enableCompletion = true;
     autocd = true;
     dotDir = ".config/zsh";
+
+    antidote = {
+      enable = true;
+      plugins = [
+        "romkatv/powerlevel10k"
+        "hlissner/zsh-autopair"
+        "Aloxaf/fzf-tab"
+      ];
+    };
 
     sessionVariables = {
       EDITOR = "vim";
@@ -14,9 +23,7 @@
 
       # pinentry for sign commits with gpg
       GPG_TTY = "$(tty)";
-
-      POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD = "true";
-
+      # gardenctl session-id
       GCTL_SESSION_ID = "$(uuidgen)";
     };
 
@@ -30,18 +37,17 @@
     };
 
     initExtra = ''
-      source ~/.config/zsh/plugins/p10k.zsh
+      # p10k
+      POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
+      source ~/.config/zsh/.p10k.zsh
 
-      # additional path config
-      export PATH=$PATH:~/bin
-      export PATH="$PATH:$HOME/.krew/bin"
-      export PATH="$PATH:~/.docker/bin"
-
+      # gardenctl config
       if command -v gardenctl &> /dev/null
       then
         source <(gardenctl rc zsh -p gctl)
       fi
 
+      # helper functions
       selc() {
         BASE_PATH=~/.config/kubeconfig
         YAMLS=$(find $BASE_PATH -name '*.yaml' | awk -F/ '{ print $NF }')
@@ -71,33 +77,6 @@
       kk = "k9s";
       clean = "nix-collect-garbage -d && nix-store --gc && nix-store --verify --check-contents --repair";
     };
-
-    plugins = with pkgs; [
-      {
-        name = "zsh-autopair";
-        src = fetchFromGitHub {
-          owner = "hlissner";
-          repo = "zsh-autopair";
-          rev = "v1.0";
-          sha256 = "1h0vm2dgrmb8i2pvsgis3lshc5b0ad846836m62y8h3rdb3zmpy1";
-        };
-        file = "autopair.zsh";
-      }
-      {
-        name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = "${zsh-nix-shell}/share/zsh-nix-shell";
-      }
-      {
-        name = "powerlevel10k";
-        file = "powerlevel10k.zsh-theme";
-        src = "${zsh-powerlevel10k}/share/zsh-powerlevel10k";
-      }
-      {
-        name = "p10k.zsh";
-        file = "p10k.zsh";
-        src = ./p10k.config;
-      }
-    ];
   };
+  home.file.".config/zsh/.p10k.zsh".source = ./p10k.config;
 }
