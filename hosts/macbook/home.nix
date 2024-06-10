e169a6e82017d56775162f08b81140273de2a401
rgroemmer <mail@rapsn.me>
2024-06-10 23:11:47 +0200

    add minimal mac setup

---
+++ hosts/macbook/home.nix
@@ -0,0 +1,22 @@
+{
+  outputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  /*
+    This is the entrypoint for home-manager
+    config is the attrs to configure all modules for HM
+  */
+  imports = [ ../../home-manager ];
+
+  config = {
+    home = {
+      username = "groemmer";
+      homeDirectory = lib.mkDefault "/Users/${config.home.username}";
+      stateVersion = outputs.stateVersion;
+    };
+  };
+}
