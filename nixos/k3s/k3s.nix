768c2af8de9c8ae5ddc399ffb32e8a3378f01f47
Raphael Groemmer <mail@rapsn.me>
2024-11-25 00:18:23 +0100

    update mount

--- nixos/k3s/k3s.nix
+++ nixos/k3s/k3s.nix
@@ -11,6 +11,7 @@
   fileSystems."/mnt/k3s_openebs_main" = {
     device = "192.168.55.10:/main_pool_z2/k3s_openebs_main";
     fsType = "nfs";
+    options = ["rw" "vers=4" "noatime"];
   };
 
   services.k3s = {
