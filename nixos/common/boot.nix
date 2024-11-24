ef19c70e91ef5430dd4b9d768085c042e20b097f
Raphael Groemmer <mail@rapsn.me>
2024-11-25 00:29:14 +0100

    remove latest kernel from common

--- nixos/common/boot.nix
+++ nixos/common/boot.nix
@@ -1,6 +1,5 @@
-{pkgs, lib, ...}: {
+{lib, ...}: {
   boot = {
-    kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
     supportedFilesystems = ["ntfs"];
     loader.efi.canTouchEfiVariables = true;
     loader.systemd-boot.enable = lib.mkDefault true;
--- nixos/k3s/k3s.nix
+++ nixos/k3s/k3s.nix
@@ -6,6 +6,7 @@
 }: {
   environment.systemPackages = with pkgs; [
     nfs-utils
+    zfs
     kubectl
   ];
 
