ff493f6c5c71556f78d701199559e177ef618331
Raphael Groemmer <mail@rapsn.me>
2024-11-30 00:27:57 +0100

    Add disko for k3s hosts

--- hosts/k3s-m2/disko.nix
+++ hosts/k3s-m2/disko.nix
@@ -1,5 +1,42 @@
 {
-  imports = [
-    ../k3s-m0/disko.nix
-  ];
+  disko.devices = {
+    disk = {
+      local = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              label = "boot";
+              name = "ESP";
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            root = {
+              size = "100%";
+              label = "root";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+                extraArgs = [
+                  "-L"
+                  "NIXOS"
+                ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
 }
