f73e43fcb9fbb61e2abced656ca845d773fdd42d
Raphael Groemmer <mail@rapsn.me>
2024-11-30 00:11:07 +0100

    fix

--- nixos/services/nh.nix
+++ nixos/services/nh.nix
@@ -3,6 +3,6 @@
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep 5 --keep-since 5d";
-    flake = /home/rap/Projects/rgroemmer/dotfiles;
+    flake = "/home/rap/Projects/rgroemmer/dotfiles";
   };
 }
