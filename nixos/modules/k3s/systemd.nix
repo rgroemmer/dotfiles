e3915f0441c97212cbbe0248732fb7efead7c8e9
Raphael Groemmer <mail@rapsn.me>
2024-12-08 01:48:24 +0100

    up

--- nixos/modules/k3s/systemd.nix
+++ nixos/modules/k3s/systemd.nix
@@ -2,13 +2,15 @@
   # Autoclone the repository
   systemd.services.git-repo-init = {
     description = "Clone a Git repository to a path if it does not exist";
-    after = ["network-online.target"]; # Ensure network is ready
+
     wantedBy = ["multi-user.target"]; # Run this at boot
+    requires = ["network-online.target"];
+    after = ["network-online.target"];
 
     serviceConfig = {
-      Type = "oneshot"; # Run the service once
+      Type = "oneshot";
       ExecStart = "${pkgs.git}/bin/git clone https://github.com/rgroemmer/dotfiles.git /home/rap";
-      ExecCondition = "! [ -d /home/rap/dotfiles ]"; # Only run if the repo isn't already cloned
+      ExecCondition = "${pkgs.coreutils}/bin/test ! [ -d /home/rap/dotfiles ]";
     };
   };
 }
