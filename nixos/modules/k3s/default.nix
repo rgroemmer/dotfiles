0b79fe40249b66c3933b34d1926eba4d45c00904
Raphael Groemmer <mail@rapsn.me>
2024-12-08 01:24:42 +0100

    Add systemd unit

--- nixos/modules/k3s/default.nix
+++ nixos/modules/k3s/default.nix
@@ -10,6 +10,7 @@ in {
   imports = [
     ./service.nix
     ./network.nix
+    ./systemd.nix
   ];
 
   options.system.modules.k3s.enable = mkEnableOption "Enable k3s cluster configuration.";
---
+++ nixos/modules/k3s/systemd.nix
@@ -0,0 +1,19 @@
+{pkgs, ...}: {
+  # Autoclone the repository
+  systemd.services.git-repo-init = {
+    description = "Clone a Git repository to a path if it does not exist";
+    after = ["network-online.target"]; # Ensure network is ready
+    wantedBy = ["multi-user.target"]; # Run this at boot
+
+    serviceConfig = {
+      Type = "oneshot"; # Run the service once
+      ExecStart = "${pkgs.git}/bin/git clone https://github.com/rgroemmer/dotfiles.git /home/rap";
+      ExecCondition = "! [ -d /home/rap/dotfiles ]"; # Only run if the repo isn't already cloned
+    };
+
+    # Ensure it only runs on fresh boots and not repeatedly
+    install = {
+      wantedBy = ["multi-user.target"];
+    };
+  };
+}
