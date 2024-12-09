44872a2f6ce481f59cff5bdc94507047b13a58ec
Raphael Groemmer <mail@rapsn.me>
2024-12-09 14:31:55 +0100

    Update k3s

--- hosts/k3s/k3s-m0/default.nix
+++ hosts/k3s/k3s-m0/default.nix
@@ -1,10 +1,16 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   imports = [
     inputs.disko.nixosModules.disko
     ../common
   ];
 
   system.modules.k3s.enable = false;
+
+  services.k3s.serverAddr = lib.mkForce "";
 
   networking = {
     hostName = "k3s-m0";
--- nixos/modules/k3s/default.nix
+++ nixos/modules/k3s/default.nix
@@ -23,14 +23,5 @@ in {
         kubectl
       ];
     };
-    system.activationScripts.mybootstrap.text = ''
-      KUBE_PATH="/home/k3s/.kube"
-      if [[ ! -f "$KUBE_PATH/config" ]]; then
-        mkdir -p $KUBE_PATH
-        ln -s /etc/rancher/k3s/k3s.yaml $KUBE_PATH/config
-        chown -R k3s:users $KUBE_PATH
-        chmod 750 $KUBE_PATH/config
-      fi
-    '';
   };
 }
