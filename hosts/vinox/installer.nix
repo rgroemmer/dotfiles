258dad458aa78e45e82ebc6236b109711722d49c
Raphael Groemmer <mail@rapsn.me>
2024-12-16 23:44:48 +0100

    Refactor installer

--- hosts/vinox/installer.nix
+++ hosts/vinox/installer.nix
@@ -6,18 +6,19 @@
     rm -rf dotfiles
     set -euo pipefail
 
+    gum style --foreground 82 --align left 'Wake up, Neo ... 💻'
+
     # Cloneing
-    gum style --border normal --margin "1" --padding "1 2" --border-foreground 218 \
-      "🐺 💿 Welcome to my $(gum style --foreground 218 'NixOS Installer'). 🙆 🐈"
-
     gum spin --show-error -s line --title "Cloning the Nixify repository..." -- \
-      git clone https://github.com/rgroemmer/dotfiles
+      git clone https://github.com/rgroemmer/dotfiles && cd dotfiles
 
-    cd dotfiles
+    gum style --foreground 40 --align left 'The Matrix has you ...'
 
     # Branch selection (for development)
     BRANCH=$(git branch -r | grep -v "\->" | awk -F'/' '{print $2}' | gum choose)
     git checkout $BRANCH
+
+    gum style --foreground 118 --italic --align left 'Follow the white rabbit. 🐰'
 
     # Choose
     ALL_CONFIGS=$(nix flake show 2>/dev/null --json | jq -r '.nixosConfigurations | keys[]' | grep -v vinox)
@@ -32,6 +33,8 @@
     # NixOS
     gum spin --show-error -s line --title "Installing NixOS configuration for $HOST" -- \
       nixos-install --flake .#$HOST
+
+    gum style --foreground 124 --bold --align left 'Knock, knock, Neo.' && sleep 1
     reboot
   '';
 in {
