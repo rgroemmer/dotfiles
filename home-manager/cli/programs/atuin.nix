2e2a0a1603cc9dc2c323d765ebdfbf0cda6e761c
Raphael Groemmer <mail@rapsn.me>
2025-01-19 12:52:34 +0100

    Add atuin with secret, update

--- home-manager/cli/programs/atuin.nix
+++ home-manager/cli/programs/atuin.nix
@@ -1,17 +1,13 @@
 {
+  sops.secrets.atuin = {
+    sopsFile = ./secrets.yaml;
+    path = "/home/rap/.config/atuin/config.toml";
+    mode = "777";
+  };
   programs.atuin = {
     enable = true;
     flags = [
       "--disable-up-arrow"
     ];
-    settings = {
-      # Defaults for new installations (`atuin default-config`)
-      sync.records = true;
-      enter_accept = true;
-
-      style = "compact";
-      inline_height = 20;
-      show_tabs = false;
-    };
   };
 }
--- nixos/services/sound.nix
+++ nixos/services/sound.nix
@@ -9,13 +9,15 @@ in {
   options.system.services.sound = mkEnableOption "Enable sound.";
 
   config = mkIf cfg {
-    hardware.pulseaudio.enable = lib.mkForce false;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
+    services = {
+      pulseaudio.enable = false;
+      pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+      };
     };
   };
 }
