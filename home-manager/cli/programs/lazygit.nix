92e71cc1785d24dbe181c469c3946d28694dff6b
Raphael Groemmer <mail@rapsn.me>
2025-01-21 11:51:18 +0100

    Reconfigure lazygit + git

---
+++ home-manager/cli/programs/lazygit.nix
@@ -0,0 +1,20 @@
+{config, ...}: {
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      git = {
+        autoFetch = false; # does not work when SSH key requires password (ref: https://github.com/jesseduffield/lazygit/issues/1498)
+        paging = {
+          colorArg = "always";
+          pager = "${config.programs.git.delta.package}/bin/delta --dark --paging=never";
+        };
+      };
+      os = {
+        editPreset = "nvim-remote";
+        edit = ''[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})'';
+        editAtLine = ''[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" &&  nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")'';
+        openDirInEditor = ''[ -z "$NVIM" ] && (nvim -- {{dir}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{dir}})'';
+      };
+    };
+  };
+}
