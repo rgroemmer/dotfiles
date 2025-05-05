4e62f5a89b829216c47ff16d613b2f4689e059ba
Raphael Groemmer <mail@rapsn.me>
2025-05-05 13:59:14 +0200

    Add temporal overlay for python mocket

--- home-manager/intoshi.nix
+++ home-manager/intoshi.nix
@@ -21,5 +21,22 @@
       homeDirectory = lib.mkDefault "/Users/${config.home.username}";
       stateVersion = lib.mkDefault "22.05";
     };
+    nixpkgs.overlays = [
+      # NOTE: Remove when python-mocket 3.13.5 is in unstable
+      (prev: rec {
+        python312 = prev.python312.override {
+          packageOverrides = prev: {
+            mocket = prev.mocket.overridePythonAttrs (oldAttrs: {
+              disabledTests =
+                oldAttrs.disabledTests
+                ++ [
+                  "test_httprettish_httpx_session"
+                ];
+            });
+          };
+        };
+        python312Packages = python312.pkgs;
+      })
+    ];
   };
 }
