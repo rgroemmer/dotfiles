cef5a0c50b7cb7602951f94e9f79187bf07a90fa
Raphael Groemmer <mail@rapsn.me>
2025-06-19 00:50:09 +0200

    Add plymouth and fast-boot for grub

--- nixos/system/boot.nix
+++ nixos/system/boot.nix
@@ -7,8 +7,8 @@ with lib; let
   cfg = config.system.boot;
 in {
   options.system.boot = {
-    systemd = mkEnableOption "Enable systemd-boot";
-    grub = mkEnableOption "Enable grub";
+    systemd = mkEnableOption "Enable systemd-boot as lightweight bootloader";
+    grub = mkEnableOption "Enable grub as beautiful bootloader";
     armSupport = mkEnableOption "Enable arm cross-compiler support";
     supportedFilesystems = mkOption {
       type = with types; listOf str;
@@ -17,11 +17,32 @@ in {
   };
 
   config = {
-    catppuccin.grub.enable = true;
+    catppuccin = mkIf cfg.grub {
+      grub.enable = true;
+      plymouth.enable = true;
+    };
 
     boot = {
       inherit (cfg) supportedFilesystems;
       binfmt.emulatedSystems = mkIf cfg.armSupport ["aarch64-linux"];
+
+      plymouth = mkIf cfg.grub {
+        enable = true;
+      };
+
+      consoleLogLevel = mkIf cfg.grub 3;
+      initrd.verbose = mkIf cfg.grub false;
+      kernelParams = mkIf cfg.grub [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "udev.log_priority=3"
+        "rd.systemd.show_status=auto"
+      ];
+      # Hide the OS choice for bootloaders.
+      # It's still possible to open the bootloader list by pressing any key
+      # It will just not appear on screen unless a key is pressed
+      loader.timeout = mkIf cfg.grub 0;
 
       loader = {
         systemd-boot.enable = cfg.systemd;
