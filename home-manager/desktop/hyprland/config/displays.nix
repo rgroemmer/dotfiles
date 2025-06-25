ae680c80b61e982644989de0cc432257b89f2436
rgroemmer <raphael.groemmer@stackit.cloud>
2025-06-25 21:27:29 +0200

    Reconfigure hyprland for work

--- home-manager/desktop/hyprland/config/displays.nix
+++ home-manager/desktop/hyprland/config/displays.nix
@@ -4,6 +4,18 @@ then {
   # Firefly
   left = {
     output = "HDMI-A-1";
+    settings = "2560x1440@144, 0x0, 1";
+  };
+  primary = {
+    output = "DP-1";
+    settings = "3840x2160@240, 2560x0, 1.5";
+  };
+  disable = {output = "eDP-1";};
+}
+else {
+  # Zion
+  left = {
+    output = "HDMI-A-1";
     settings = "highres, 0x0, 1";
   };
   primary = {
