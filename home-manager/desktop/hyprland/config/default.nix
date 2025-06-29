7db4cc2f7415ca61f3a8f3abc2f1d0be93dd7da5
rgroemmer <github@rapsn.me>
2025-06-30 00:10:53 +0200

    Add blur & opacity

--- home-manager/desktop/hyprland/config/default.nix
+++ home-manager/desktop/hyprland/config/default.nix
@@ -73,7 +73,16 @@ in {
     };
 
     decoration = {
+      blur = {
+        enabled = true;
+        size = 3;
+        passes = 2;
+        ignore_opacity = true;
+        new_optimizations = true;
+      };
       rounding = 5;
+      active_opacity = 0.95;
+      inactive_opacity = 0.85;
     };
   };
 }
