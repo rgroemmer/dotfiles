68ec0db5aa61b7d225046992f62ddc8209f87862
rgroemmer <github@rapsn.me>
2025-06-30 00:10:46 +0200

    Adjust hyprlock

--- home-manager/desktop/hyprland/addons/hyprlock.nix
+++ home-manager/desktop/hyprland/addons/hyprlock.nix
@@ -22,12 +22,12 @@
       listener = [
         # autolock
         {
-          timeout = 120;
+          timeout = 1200;
           on-timeout = "hyprlock";
         }
         # display off
         {
-          timeout = 1200;
+          timeout = 9000;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
